d566ac87f359021c02f9c852bab45e18029d94a4
MoriFGC <abdulmoha@outlook.it>
2024-05-16 21:21:33 +0200

    commit

--- Album/album.js
+++ Album/album.js
@@ -1,37 +1,57 @@
-const api ="https://striveschool-api.herokuapp.com/api/deezer/search?q=";
-let filter=`salmo`;
+const api ="https://striveschool-api.herokuapp.com/api/deezer/Album/";
+const newUrl = new URLSearchParams(location.search);
+const id = newUrl.get("id");
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjNjY2M2NzgxODQ0MjAwMTUzNzU3NWIiLCJpYXQiOjE3MTU2MjI2NTEsImV4cCI6MTcxNjgzMjI1MX0.XPysuu2j4g-SV_E20G-RY5PanUO8Qv0TVe0HbwzGZ-Y"
 
 async function chiamataFetch() {
 
-    const response = await fetch(api + filter, {
+    let totSec = 0;
+    let min = 0;
+    let sec = 0;
+
+    const response = await fetch(api + id, {
         headers: {
             'Authorization': `Bearer ${token}`
         }
     });
     const data = await response.json();
-    const fotoProfilo = document.getElementById(`foto-profilo`);
-    const titoloAlbum = document.getElementById(`album-title`);
-    const artistName = document.getElementById(`artist-name`);
-    const artistProfile = document.getElementById(`artist-profile`);
-    const filterInput = document.getElementById(`filter-input`).value.toLowerCase();
-    if(fotoProfilo){
-        fotoProfilo.innerHTML = `<img src="${data.data[0].album.cover_big}" alt="" style="width: 100%; ">`;
-        titoloAlbum.innerHTML = `${data.data[0].album.title}`;
-        artistName.innerHTML = `${data.data[0].artist.name}`;
-        artistProfile.innerHTML = `
-            <img src="${data.data[0].artist.picture_small}" alt="">
-            <span class="fw-bold">${data.data[0].artist.name}</span>
-            <span>•</span>
-            <span class="fw-bold">2017</span>
-            <span>•</span>
-            <span class="fw-bold">12 Brani, </span>
-            <span>53 min 20 sec</span>
-        `;
+    console.log(data);
+    totSec+=parseInt(data.duration);
+    min = parseInt(totSec/60);
+    sec = parseInt(totSec - (min*60));
+    const hero = document.getElementById('hero');
+    hero.innerHTML = `<div class="col-3" id="foto-profilo">
+                        <img src="${data.cover_medium}" alt="" style="width: 100%">
+                       </div>
+                        <div class="col-9 mb-5">
+                            <span class="fw-bold">Album</span>
+                            <h1 id="album-title"></h1>
+                            <div class="dettagli-hero" id="artist-profile">
+                                <img src="img-user/Immagine WhatsApp 2023-11-19 ore 18.04.26_0543847ss3.jpg" alt="">
+                                <span class="fw-bold" id="artist-name">${data.artist.name}</span>
+                                <span>•</span>
+                                <span class="fw-bold">2017</span>
+                                <span>•</span>
+                                <span class="fw-bold">${data.nb_tracks} Brani, </span>
+                                <span>${min} min ${sec} sec</span>
+                            </div>
+                        </div>`;
+
+    const tracks = document.getElementById('lista-canzoni');
+    data.tracks.data.map((traccia) => {
+        tracks.innerHTML += 
+        `
+                <tr>
+                  <th class="col-1">1</th>
+                  <td class="col-7">${traccia.title}</td>
+                  <td class="col-2">${traccia.rank}</td>
+                  <td class="col-2">${traccia.duration}</td>
+                </tr>
+        `
+    });
 
     }
-    console.log(data);
-}
+
 chiamataFetch();
 
 //PER CALCOLARE I MINUTI (E I SECONDI) PRENDIAMO LA DURATA, DIVIDIAMO PER 60 E POI PRENDIAMO  
